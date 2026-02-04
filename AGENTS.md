1f3d98edfc0b9d83689a3acb969e220f38b75cf3
Niek Schmoller <niek.schmoller@gmail.com>
2026-02-04 11:10:23 +0100

    first step updating project and replace fontawesome with cdn version

---
+++ AGENTS.md
@@ -0,0 +1,29 @@
+# Project Agent Notes
+
+## Run locally
+
+1. Install dependencies:
+
+```bash
+npm install
+```
+
+2. Start the dev server (BrowserSync):
+
+```bash
+gulp dev
+```
+
+Run `gulp dev` in a separate terminal and keep it running.
+
+## Build assets
+
+```bash
+gulp css
+gulp js
+```
+
+## Notes
+
+- This is a static site. Main entry is `index.html`.
+- Gulp tasks are defined in `gulpfile.js`.
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+var sass = require('gulp-sass')(require('sass'));
 var header = require('gulp-header');
 var cleanCSS = require('gulp-clean-css');
 var rename = require("gulp-rename");
@@ -17,43 +17,34 @@ var banner = ['/*!\n',
 ].join('');
 
 // Copy third party libraries from /node_modules into /vendor
-gulp.task('vendor', function() {
-
-  // Bootstrap
-  gulp.src([
+function vendorBootstrap() {
+  return gulp.src([
       './node_modules/bootstrap/dist/**/*',
       '!./node_modules/bootstrap/dist/css/bootstrap-grid*',
       '!./node_modules/bootstrap/dist/css/bootstrap-reboot*'
     ])
-    .pipe(gulp.dest('./vendor/bootstrap'))
+    .pipe(gulp.dest('./vendor/bootstrap'));
+}
 
-  // Font Awesome
-  gulp.src([
-      './node_modules/font-awesome/**/*',
-      '!./node_modules/font-awesome/{less,less/*}',
-      '!./node_modules/font-awesome/{scss,scss/*}',
-      '!./node_modules/font-awesome/.*',
-      '!./node_modules/font-awesome/*.{txt,json,md}'
-    ])
-    .pipe(gulp.dest('./vendor/font-awesome'))
-
-  // jQuery
-  gulp.src([
+function vendorJquery() {
+  return gulp.src([
       './node_modules/jquery/dist/*',
       '!./node_modules/jquery/dist/core.js'
     ])
-    .pipe(gulp.dest('./vendor/jquery'))
+    .pipe(gulp.dest('./vendor/jquery'));
+}
 
-  // jQuery Easing
-  gulp.src([
+function vendorJqueryEasing() {
+  return gulp.src([
       './node_modules/jquery.easing/*.js'
     ])
-    .pipe(gulp.dest('./vendor/jquery-easing'))
+    .pipe(gulp.dest('./vendor/jquery-easing'));
+}
 
-});
+var vendor = gulp.parallel(vendorBootstrap, vendorJquery, vendorJqueryEasing);
 
 // Compile SCSS
-gulp.task('css:compile', function() {
+function cssCompile() {
   return gulp.src('./scss/**/*.scss')
     .pipe(sass.sync({
       outputStyle: 'expanded'
@@ -61,11 +52,11 @@ gulp.task('css:compile', function() {
     .pipe(header(banner, {
       pkg: pkg
     }))
-    .pipe(gulp.dest('./css'))
-});
+    .pipe(gulp.dest('./css'));
+}
 
 // Minify CSS
-gulp.task('css:minify', ['css:compile'], function() {
+function cssMinify() {
   return gulp.src([
       './css/*.css',
       '!./css/*.min.css'
@@ -76,13 +67,13 @@ gulp.task('css:minify', ['css:compile'], function() {
     }))
     .pipe(gulp.dest('./css'))
     .pipe(browserSync.stream());
-});
+}
 
 // CSS
-gulp.task('css', ['css:compile', 'css:minify']);
+var css = gulp.series(cssCompile, cssMinify);
 
 // Minify JavaScript
-gulp.task('js:minify', function() {
+function jsMinify() {
   return gulp.src([
       './js/*.js',
       '!./js/*.min.js'
@@ -96,26 +87,38 @@ gulp.task('js:minify', function() {
     }))
     .pipe(gulp.dest('./js'))
     .pipe(browserSync.stream());
-});
+}
 
 // JS
-gulp.task('js', ['js:minify']);
-
-// Default task
-gulp.task('default', ['css', 'js', 'vendor']);
+var js = gulp.series(jsMinify);
 
 // Configure the browserSync task
-gulp.task('browserSync', function() {
+function browserSyncServe(done) {
   browserSync.init({
     server: {
       baseDir: "./"
     }
   });
-});
+  done();
+}
+
+function browserSyncReload(done) {
+  browserSync.reload();
+  done();
+}
 
 // Dev task
-gulp.task('dev', ['css', 'js', 'browserSync'], function() {
-  gulp.watch('./scss/*.scss', ['css']);
-  gulp.watch('./js/*.js', ['js']);
-  gulp.watch('./*.html', browserSync.reload);
-});
+function dev() {
+  gulp.watch('./scss/*.scss', css);
+  gulp.watch('./js/*.js', js);
+  gulp.watch('./*.html', browserSyncReload);
+}
+
+var build = gulp.parallel(css, js, vendor);
+
+exports.vendor = vendor;
+exports.css = css;
+exports.js = js;
+exports.build = build;
+exports.default = build;
+exports.dev = gulp.series(build, browserSyncServe, dev);
